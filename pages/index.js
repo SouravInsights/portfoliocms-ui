be923711448d343630cf415732e6349391c2d8a1
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2021-01-20 02:00:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,45 +9,11 @@ import { Projects } from "../components/Projects";
 import { Header, Footer } from "../components/Layout";
 
 function App() {
-  const [preview, setPreview] = useState("dashboard");
-
-  const changePreview = (e) => {
-    setPreview(e.target.value);
-  };
-  let defaultComponent;
-  const previewComponent = () => {
-    switch (preview) {
-      case "dashboard":
-        defaultComponent = <Dashboard />;
-        break;
-      case "projects":
-        defaultComponent = <Projects />;
-        break;
-    }
-    return defaultComponent;
-  };
   return (
     <ChakraProvider theme={theme}>
       <Header />
-      <Box
-        position="absolute"
-        bg="#ffffff"
-        borderWidth="1px"
-        borderColor="#cccccc"
-        bottom="30%"
-        zIndex="10"
-        p={4}
-      >
-        <Text fontWeight="500">Preview:</Text>
-        <Select onChange={changePreview}>
-          <option selected value="dashboard">
-            Dashboard
-          </option>
-          <option value="projects">Table</option>
-        </Select>
-      </Box>
-      {previewComponent()}
-
+      <Dashboard />
+      <Projects />
       <Footer />
     </ChakraProvider>
   );
